7ace32f877c6b1a72ecf08079d097151c9d4ac4c
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-20 13:33:39 +0200

    Add Common project

---
+++ MamaStalker/Common/DTOs/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Common.DTOs
+{
+    [Serializable]
+    public class Person
+    {
+        private string _name;
+        private int _age;
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{_name} is {_age} years old.";
+        }
+    }
+}
