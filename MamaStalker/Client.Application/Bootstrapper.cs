6b74a86de65ab40cd89735dbc1d26a54db98ae64
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-20 13:37:05 +0200

    Add Client.Application project

---
+++ MamaStalker/Client.Application/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using Client.BLL.Abstractions;
+using Client.BLL.Implementations;
+using UI.Abstractions;
+using UI.Implementations;
+
+namespace Client.Application
+{
+    public class Bootstrapper
+    {
+        private const int _minPort = 0;
+        private const int _maxPort = 65536;
+
+        public ClientBase Initialize()
+        {
+            IClientFactory clientFactory = new TcpClientManagerFactory();
+            IOutput<string> writer = new ConsoleWriter();
+            IInput<string> reader = new ConsoleReader();
+            NotifyException notifyException = new NotifyException(writer);
+            int port = -1;
+            string ip = string.Empty;
+            while(port < _minPort || port > _maxPort)
+            {
+                writer.WriteLine("Enter ip:port");
+                string input = reader.ReadLine();
+                ip = input.Substring(0, input.IndexOf(":"));
+                port = int.Parse(input.Substring(input.IndexOf(":") + 1));
+            }
+            return clientFactory.CreateClient(port, ip, notifyException); 
+        }
+    }
+}
---
+++ MamaStalker/Client.Application/Program.cs
@@ -0,0 +1,16 @@
+﻿using Client.BLL.Abstractions;
+
+namespace Client.Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Bootstrapper bootstrapper = new Bootstrapper();
+
+            ClientBase client = bootstrapper.Initialize();
+
+            client.Run();
+        }
+    }
+}
